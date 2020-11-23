9fad188d190bba60ef77c995be80ead0ba48ac08
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2020-11-23 23:47:30 +0500

    Update generation_utils.py

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -695,14 +695,14 @@ class GenerationMixin:
             input_ids, max_length
         )
 
-        dicts = []
+        cross_attentions = []
         while cur_len < max_length:
             # prepare model inputs
             model_inputs = self.prepare_inputs_for_generation(input_ids, **model_kwargs)
 
             # forward pass to get next token
             outputs = self(**model_inputs, output_attentions = True, return_dict=True)
-            dicts.append(outputs)
+            cross_attentions.append(outputs.cross_attentions)
             next_token_logits = outputs.logits[:, -1, :]
 
             # pre-process distribution
@@ -737,7 +737,7 @@ class GenerationMixin:
             # increase cur_len
             cur_len = cur_len + 1
 
-        return input_ids, dicts
+        return input_ids, cross_attentions
 
     def sample(
         self,
